c11ad24b0b9834c878ca5178ecbd2e9c71238278
atsushieno <atsushieno@gmail.com>
2021-09-29 17:07:21 +0900

    release 0.1.5.

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ configurations.matching { it.name != "kotlinCompilerPluginClasspath" }.all {
 
 allprojects {
     group = "dev.atsushieno"
-    version = "0.1.4"
+    version = "0.1.5"
 
     repositories {
         mavenLocal()
